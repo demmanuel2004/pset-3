e3fb685e34e636cee9f547c012a21829b863754f
Daniel Emmanuel <[DEmmanuel@ucvts.tec.nj.us]>
2019-10-30 17:52:11 -0400

    finished parity

--- src/parity.js
+++ src/parity.js
@@ -1,13 +1,15 @@
 const readlineSync = require("readline-sync");
 const num = Number(readlineSync.question("\nEnter a number: "));
 
-if (Number.isNaN(num)){
-  console.log("\nInvalid.\n");
-}
-
 if ( num % 2 ==0) {
 	console.log("\nEven.\n");
 }
+
+
+else if (Number.isNaN(num)){
+  console.log("\nInvalid.\n");
+}
+
 else{
   console.log("\nOdd.\n");
 }
