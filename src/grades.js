055b053de3272f27e9278545036cffa4b9fb3646
demmanuel2004 <55405293+demmanuel2004@users.noreply.github.com>
2019-10-31 11:23:30 -0400

    Created and finished grades

---
+++ src/grades.js
@@ -0,0 +1,22 @@
+const readlineSync = require("readline-sync");
+const x = Number(readlineSync.question("\nEnter a number: "));
+
+
+if (x >= 90 && x <= 100 )  {
+	console.log("\nYou recieved an A.\n");
+}
+else if (x >= 80 && x <= 89 )  {
+	console.log("\nYou recieved an B.\n");
+}
+else if (x >= 70 && x <= 79 )  {
+	console.log("\nYou recieved an C.\n");
+}
+else if (x >= 60 && x <= 69 )  {
+	console.log("\nYou recieved an D.\n");
+}
+else if (x >= 0 && x <= 59 )  {
+	console.log("\nYou recieved an F.\n");
+}
+else if (Number.isNaN(x)){
+  console.log("\nInvalid.\n");
+}
