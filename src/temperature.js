2d158267a56d643d6fb62523c8bfd3e4b45d49da
Daniel Emmanuel <[DEmmanuel@ucvts.tec.nj.us]>
2019-11-02 12:28:39 -0400

    finished temperature and edits

---
+++ src/temperature.js
@@ -0,0 +1,41 @@
+const readlineSync = require("readline-sync");
+const temp = Number(readlineSync.question("\nEnter a temperature: "));
+const scale = String(readlineSync.question("\nEnter a scale: "));
+
+
+
+if (temp >= 32 && scale == "F")  {
+	console.log("\nLiquid.\n");
+}
+
+else if (temp >= 212 && scale == "F" )  {
+	console.log("\nGas.\n");
+}
+
+else if (temp <= 32 && scale == "F" )  {
+	console.log("\nSolid.\n");
+}
+
+if (temp >= 273 && scale == "K")  {
+	console.log("\nLiquid.\n");
+}
+
+else if (temp >= 373 && scale == "K" )  {
+	console.log("\nGas.\n");
+}
+
+else if (temp <= 273 && scale == "K" )  {
+	console.log("\nSolid.\n");
+}
+
+if (temp >= 0 && scale == "C")  {
+	console.log("\nLiquid.\n");
+}
+
+else if (temp >= 100 && scale == "C" )  {
+	console.log("\nGas.\n");
+}
+
+else if (temp <= 0 && scale == "C" )  {
+	console.log("\nSolid.\n");
+}
