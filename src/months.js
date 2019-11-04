1e2d17ff612365f355adc5807f7156407e1f3bcb
Daniel Emmanuel <[DEmmanuel@ucvts.tec.nj.us]>
2019-11-04 18:01:27 -0500

    edits to a few problems

--- src/months.js
+++ src/months.js
@@ -1,51 +1,51 @@
 const readlineSync = require("readline-sync");
 const month = String(readlineSync.question("\nEnter a month: "));
 
-if (month == "jan")  {
+if (month == "jan" || month == "Jan" || month == "JAN")  {
  console.log("\n31 days.\n");
 }
 
-else if (month == "feb" )  {
+else if (month == "feb" || month == "Feb" || month == "FEB")  {
  console.log("\n28 or 29 days.\n");
 }
 
-else if (month == "mar")  {
+else if (month == "mar" || month == "Mar" || month == "MAR")  {
   console.log("\n31 days.\n");
 }
 
-else if (month == "apr")  {
+else if (month == "apr" || month == "Apr" || month == "APR")  {
  console.log("\n30 days.\n");
 }
 
-else if (month == "may")  {
+else if (month == "may" || month == "May" || month == "MAY")  {
  console.log("\n31 days.\n");
 }
 
-else if (month == "jun") {
+else if (month == "jun" || month == "Jun" || month == "JUN") {
 	console.log("\n30 days")
 }
 
-else if (month == "jul")  {
+else if (month == "jul" || month == "Jul" || month == "JUL")  {
  console.log("\n31 days.\n");
 }
 
-else if (month == "aug")  {
+else if (month == "aug" || month == "Aug" || month == "AUG")  {
  console.log("\n31 days.\n");
 }
 
-else if (month == "sept")  {
+else if (month == "sept" || month == "Sept" || month == "SEPT")  {
  console.log("\n30 days.\n");
 }
 
-else if (month == "oct")  {
+else if (month == "oct" || month == "Oct" || month == "OCT")  {
  console.log("\n31 days.\n");
 }
 
-else if (month == "nov")  {
+else if (month == "nov" || month == "Nov" || month == "NOV")  {
  console.log("\n30 days.\n");
 }
 
-else if (month == "dec")  {
+else if (month == "dec" || month == "Dec" || month == "DEC")  {
  console.log("\n31 days.\n");
 }
 
--- src/sequence.js
+++ src/sequence.js
@@ -1,1 +1,36 @@
+const readlineSync = require("readline-sync");
 
+console.log("\nEnter three numbers" );
+
+const INTEGER = Number(readlineSync.question("\n"));
+const SECOND_INTEGER = Number(readlineSync.question(""));
+const THIRD_INTEGER = Number(readlineSync.question(""));
+
+
+if (Number.isNaN(INTEGER)) {
+    console.log("\nInvalid.");
+}
+
+else if (INTEGER == SECOND_INTEGER && SECOND_INTEGER == THIRD_INTEGER) {
+    console.log("\nEqual.");
+}
+
+else if (INTEGER - SECOND_INTEGER == SECOND_INTEGER - THIRD_INTEGER && THIRD_INTEGER > SECOND_INTEGER == SECOND_INTEGER > INTEGER) {
+  console.log("\nStrictly increasing");
+}
+
+else if (INTEGER < SECOND_INTEGER && SECOND_INTEGER < THIRD_INTEGER) {
+  console.log("\nIncreasing.");
+}
+
+else if (THIRD_INTEGER - SECOND_INTEGER == SECOND_INTEGER - INTEGER) {
+  console.log("\nStrictly decreasing.");
+  }
+
+else if (INTEGER > SECOND_INTEGER && SECOND_INTEGER > THIRD_INTEGER) {
+  console.log("\nDecreasing");
+}
+
+else {
+ console.log("\nUnordered.");
+}
