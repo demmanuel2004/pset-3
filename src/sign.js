de56bd1338b328d5d08045119ccaa4b50bdf6084
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2019-11-06 09:39:17 -0500

    Update sign.js

--- src/sign.js
+++ src/sign.js
@@ -1,6 +1,9 @@
 const readlineSync = require("readline-sync");
 const NUM = Number(readlineSync.question("\nEnter a number: "));
 
+const MIN = Number.MIN_SAFE_INTEGER ;
+const MAX = Number.MAX_SAFE_INTEGER ;
+
 
 if ( NUM > 0) {
   console.log("\npositive.\n");``
