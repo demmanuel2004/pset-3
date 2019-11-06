fbcb767a2fc1b1f1c3f3133b9deff9110faf6d7d
Daniel Emmanuel <55405293+demmanuel2004@users.noreply.github.com>
2019-11-05 20:56:49 -0500

    Update months.js

--- src/months.js
+++ src/months.js
@@ -1,53 +1,66 @@
 const readlineSync = require("readline-sync");
-const month = String(readlineSync.question("\nEnter a month: "));
+const MONTH = String(readlineSync.question("\nEnter a MONTH: "));
 
-if (month == "jan" || month == "Jan" || month == "JAN")  {
+
+if (MONTH == "jan" || MONTH == "Jan" || MONTH == "JAN")  {
  console.log("\n31 days.\n");
 }
 
-else if (month == "feb" || month == "Feb" || month == "FEB")  {
+
+else if (MONTH == "feb" || MONTH == "Feb" || MONTH == "FEB")  {
  console.log("\n28 or 29 days.\n");
 }
 
-else if (month == "mar" || month == "Mar" || month == "MAR")  {
+
+else if (MONTH == "mar" || MONTH == "Mar" || MONTH == "MAR")  {
   console.log("\n31 days.\n");
 }
 
-else if (month == "apr" || month == "Apr" || month == "APR")  {
+
+else if (MONTH == "apr" || MONTH == "Apr" || MONTH == "APR")  {
  console.log("\n30 days.\n");
 }
 
-else if (month == "may" || month == "May" || month == "MAY")  {
+
+else if (MONTH == "may" || MONTH == "May" || MONTH == "MAY")  {
  console.log("\n31 days.\n");
 }
 
-else if (month == "jun" || month == "Jun" || month == "JUN") {
+
+else if (MONTH == "jun" || MONTH == "Jun" || MONTH == "JUN") {
 	console.log("\n30 days")
 }
 
-else if (month == "jul" || month == "Jul" || month == "JUL")  {
+
+else if (MONTH == "jul" || MONTH == "Jul" || MONTH == "JUL")  {
  console.log("\n31 days.\n");
 }
 
-else if (month == "aug" || month == "Aug" || month == "AUG")  {
+
+else if (MONTH == "aug" || MONTH == "Aug" || MONTH == "AUG")  {
  console.log("\n31 days.\n");
 }
 
-else if (month == "sept" || month == "Sept" || month == "SEPT")  {
+
+else if (MONTH == "sept" || MONTH == "Sept" || MONTH == "SEPT")  {
  console.log("\n30 days.\n");
 }
 
-else if (month == "oct" || month == "Oct" || month == "OCT")  {
+
+else if (MONTH == "oct" || MONTH == "Oct" || MONTH == "OCT")  {
  console.log("\n31 days.\n");
 }
 
-else if (month == "nov" || month == "Nov" || month == "NOV")  {
+
+else if (MONTH == "nov" || MONTH == "Nov" || MONTH == "NOV")  {
  console.log("\n30 days.\n");
 }
 
-else if (month == "dec" || month == "Dec" || month == "DEC")  {
+
+else if (MONTH == "dec" || MONTH == "Dec" || MONTH == "DEC")  {
  console.log("\n31 days.\n");
 }
+
 
 else {
  console.log("\nInvalid.\n");
